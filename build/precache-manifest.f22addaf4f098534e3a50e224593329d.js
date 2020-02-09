b3e4b87db23da5e24837d1e490d1f3d9e8bb890c
szymon <szymon.kuc@poczta.onet.pl>
2020-02-09 20:42:26 +0100

    testing build

---
+++ build/precache-manifest.f22addaf4f098534e3a50e224593329d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc5bf37e3b36f7b28428",
+    "url": "/spending_summary/static/js/runtime~main.cc5bf37e.js"
+  },
+  {
+    "revision": "8c00ecc2b9ca8fc901da",
+    "url": "/spending_summary/static/js/main.8c00ecc2.chunk.js"
+  },
+  {
+    "revision": "fe3d8e1acdbd67c91743",
+    "url": "/spending_summary/static/js/2.fe3d8e1a.chunk.js"
+  },
+  {
+    "revision": "8c00ecc2b9ca8fc901da",
+    "url": "/spending_summary/static/css/main.92e7e719.chunk.css"
+  },
+  {
+    "revision": "425212528a4e2ab874e11d1d0cd86179",
+    "url": "/spending_summary/index.html"
+  }
+];
